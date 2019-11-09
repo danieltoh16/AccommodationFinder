367d1024d2d1cba5c988adfdc403553c98c2e6ec
danieltoh16 <40682209+danieltoh16@users.noreply.github.com>
2019-11-09 17:16:21 +0800

    Failed attempt to check if user is registering with same username/email

--- signUpAction.php
+++ signUpAction.php
@@ -9,9 +9,6 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     echo("Connection failed: " . $conn->connect_error);
 }
-else{
-  echo("Success");
-}
 
 $conn->select_db("accommodationfinder");
 
@@ -23,16 +20,24 @@ $monthIncome = $_POST['mincome'];
 
 $wrongMsg = "Sign up unsuccessful. Please try again.";
 $msg = "Sign up successful. Proceed to login.";
+$uNameError = "This username is already taken. Please try another username";
+$emailError = "This email is already taken. Please try another email";
 
 $sql = "INSERT INTO users (`staffID`, `usertype`, `username`, `password`, `fullName`, `email`, `monthlyIncome`) VALUES ('', 1, '$uName', '$pass', '$fullName', '$email', '$monthIncome')";
+$sql_uName = "SELECT COUNT (*) FROM users WHERE username LIKE '$uName'";
+$sql_email = "SELECT COUNT (*) FROM users WHERE email LIKE '$email'";
 
-if ($conn->query($sql) === TRUE) {
-	echo "<SCRIPT>alert('$msg');</SCRIPT>";
-	echo("<SCRIPT type=\"text/javascript\">window.location = 'login.html';</SCRIPT>");
-}
-else if ($result && $result->num_rows){
-  echo "<SCRIPT>alert('$msg');</SCRIPT>";
-	echo("<SCRIPT type=\"text/javascript\">window.location = 'login.html';</SCRIPT>");
+if (($conn->query($sql) === TRUE)||($result && $result->num_rows)) {
+	if ($sql_uName > 0){
+    echo "<SCRIPT>alert('$sql_uName');</SCRIPT>";
+    echo("<SCRIPT type=\"text/javascript\">window.location = 'signUp.html';</SCRIPT>");
+  } else if ($sql_email > 0){
+    echo "<SCRIPT>alert('$sql_email');</SCRIPT>";
+    echo("<SCRIPT type=\"text/javascript\">window.location = 'signUp.html';</SCRIPT>");
+  } else {
+    echo "<SCRIPT>alert('$msg');</SCRIPT>";
+    echo("<SCRIPT type=\"text/javascript\">window.location = 'login.html';</SCRIPT>");
+  }
 }
 else {
 	echo "<SCRIPT>alert('$wrongMsg');</SCRIPT>";
